75e16e2c155266354286f0a2e83f83ce047a17e8
dokimiki <57443812+dokimiki@users.noreply.github.com>
2024-07-08 17:43:23 +0900

    🗑️ 不要なファイルを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,25 @@
-import { GeistSans } from "geist/font/sans";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = process.env.VERCEL_URL !== undefined ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+    metadataBase: new URL(defaultUrl),
+    title: "Next.js and Supabase Starter Kit",
+    description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+const notoSansJP = Noto_Sans_JP({
+    subsets: ["latin"],
+    variable: "--font-noto-sans-jp",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
+    return (
+        <html className={notoSansJP.className} lang="ja">
+            <body>
+                <main>{children}</main>
+            </body>
+        </html>
+    );
 }
