520dd3977787a7b690468c8307e5388e20b8be5e
dokimiki <57443812+dokimiki@users.noreply.github.com>
2024-07-18 21:34:29 +0900

    全部治した！！

--- src/libs/manabo.ts
+++ src/libs/manabo.ts
@@ -1,54 +1,53 @@
 import aspida, { type FetchConfig } from "@aspida/fetch";
-import { AspidaClient } from "aspida";
-import api from "../api/$api";
-import consola from "consola";
-import { ClassInfo } from "../types/common";
+import api from "@/src/api/$api";
+import { type ClassInfo } from "@/src/types/common";
 
-interface ProxyRequest{
+type ProxyRequest = {
     body: {
         path: string;
         method: string;
         authorization?: string | undefined;
         content_type?: string | undefined;
         body?: string | undefined;
-    }; config?: FetchConfig | undefined;
-}
+    };
+    config?: FetchConfig | undefined;
+};
 
 export async function getClassList(): Promise<ClassInfo[]> {
-    const body = (await fetchManabo({
+    const { body } = await fetchManabo({
         body: {
             path: "/",
             method: "POST",
             content_type: "application/x-www-form-urlencoded",
-            body: "action=glexa_ajax_timetable_view"
-        }
-    })).body;
-    let timeTable: ClassInfo[] = [];
+            body: "action=glexa_ajax_timetable_view",
+        },
+    });
+    // const timeTable: ClassInfo[] = [];
     const parser = new DOMParser();
     const doc = parser.parseFromString(body, "text/html");
     const tables = doc.querySelectorAll("tbody");
     const domTimeTable = tables[0];
-    const domAnotherClass = tables[1];
+    // const domAnotherClass = tables[1];
 
     return parseTimeTable(domTimeTable);
 }
 function parseTimeTable(dom: HTMLTableSectionElement): ClassInfo[] {
-    let timeTable: ClassInfo[] = [];
+    const timeTable: ClassInfo[] = [];
     let period = 0;
     const trs = dom.querySelectorAll("tr");
     for (const tr of trs) {
         const columns = tr.querySelectorAll("td");
-        let day = 1;//月曜日
+        let day = 1; // 月曜日
         for (const column of columns) {
             const infoDom = column.querySelector(".student");
-            if (infoDom) {
-                const teacher = (infoDom.innerHTML + "<").replaceAll(/\t|\s/g, "").match(/<br>(.*?)</)![1];
+            if (infoDom !== null) {
+                const teacher = `${infoDom.innerHTML}<`.replaceAll(/\t|\s/g, "").match(/<br>(.*?)</)?.[1] ?? "unknown";
 
                 timeTable.push({
                     dayOfWeek: day,
-                    period: period,
-                    teacher: teacher,
-                    title: infoDom.querySelector("b")?.textContent!
+                    period,
+                    teacher,
+                    title: infoDom.querySelector("b")?.textContent ?? "わからないやつ",
                 });
             }
             day++;
@@ -58,17 +57,14 @@ function parseTimeTable(dom: HTMLTableSectionElement): ClassInfo[] {
     return timeTable;
 }
 
-async function fetchManabo(req: ProxyRequest) {
+async function fetchManabo(req: ProxyRequest): Promise<{ status_code: number; body: string; crumbed_cookie: string }> {
     const COOKIE_KEY = "manato:crumbed_cookie";
-    const crumbed_cookie = localStorage.getItem(COOKIE_KEY);
+    const crumbedCookie = localStorage.getItem(COOKIE_KEY);
     const request: ProxyRequest & { body: { crumbed_cookie?: string | null } } = req;
-    request.body.crumbed_cookie = crumbed_cookie;
-    
-    const apiClient = api(aspida(fetch, { throwHttpErrors: false, mode: "cors" }) as AspidaClient<FetchConfig>);
+    request.body.crumbed_cookie = crumbedCookie;
 
+    const apiClient = api(aspida(fetch, { throwHttpErrors: false, mode: "cors" }));
+
+    // eslint-disable-next-line @typescript-eslint/return-await
     return apiClient.v1.manabo.proxy.$post(req);
-    
 }
-
-
-
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,8 @@
 export type DayOfWeek = "sun" | "mon" | "tue" | "wed" | "thu" | "fri" | "sat";
 
-export interface ClassInfo{
-    dayOfWeek: number,
-    period: number,
-    teacher: string,
-    title: string,
-}
+export type ClassInfo = {
+    dayOfWeek: number;
+    period: number;
+    teacher: string;
+    title: string;
+};
